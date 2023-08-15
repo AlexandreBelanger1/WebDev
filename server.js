20c181e544259b7b738b10df28ece6d9a5156325
AlexandreBelanger1 <alexandrebelanger515@gmail.com>
2023-08-15 10:11:53 -0600

    changed server file to host multiple html files changed server file to host multiple html files

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
+});
+*/
+
+http.createServer((req,res)=>{
+    var q = url.parse(req.url,true);
+    var filename = "."+q.pathname;
+    fs.readFile(filename,function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
 });
 
 app.listen(port);
