834738f764bbe05d348d82a69fe2e9b1e13fd103
AlexandreBelanger1 <Alexandrebelanger515@gmail.com>
2023-08-15 12:15:15 -0600

    Functional multi-html page support

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+const router = express.Router();
+
+// sendFile will go here
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.get('/index.html',(req, res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.get('/contact.html',(req, res)=>{
+    res.sendFile(path.join(__dirname+'/contact.html'));
+});
+app.get('/Resume.html',(req, res)=>{
+    res.sendFile(path.join(__dirname+'/Resume.html'));
+});
+
+app.use('/', router);
+app.listen(port, function(err){
+    if (err) console.log("Error in server setup")
+    console.log('Server listening at ' + port);
+});
